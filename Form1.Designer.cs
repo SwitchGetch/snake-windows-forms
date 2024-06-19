8ad5cc0689d2eb44f51d2d769e2c1ea5b13cc4f4
GetChara() <133586791+SwitchGetch@users.noreply.github.com>
2024-06-19 17:23:02 +0300

    Update Form1.Designer.cs

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -1,109 +1,82 @@
 ﻿namespace WinFormsApp3
 {
-    public partial class Form1
-    {
-        /// <summary>
-        ///  Required designer variable.
-        /// </summary>
-        private System.ComponentModel.IContainer components = null;
+     public partial class Form1
+     {
+     /// <summary>
+     ///  Required designer variable.
+     /// </summary>
+     private System.ComponentModel.IContainer components = null;
 
-        /// <summary>
-        ///  Clean up any resources being used.
-        /// </summary>
-        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
-        protected override void Dispose(bool disposing)
-        {
-            if (disposing && (components != null))
-            {
-                components.Dispose();
-            }
-            base.Dispose(disposing);
-        }
+     /// <summary>
+     ///  Clean up any resources being used.
+     /// </summary>
+     /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+     protected override void Dispose(bool disposing)
+     {
+         if (disposing && (components != null))
+         {
+             components.Dispose();
+         }
+         base.Dispose(disposing);
+     }
 
-        #region Windows Form Designer generated code
+     #region Windows Form Designer generated code
 
-        /// <summary>
-        ///  Required method for Designer support - do not modify
-        ///  the contents of this method with the code editor.
-        /// </summary>
-        private void InitializeComponent()
-        {
-            components = new System.ComponentModel.Container();
-            tableLayoutPanel1 = new TableLayoutPanel();
-            timer1 = new System.Windows.Forms.Timer(components);
-            SuspendLayout();
-            // 
-            // tableLayoutPanel1
-            // 
-            tableLayoutPanel1.ColumnCount = 20;
-            tableLayoutPanel1.ColumnStyles.Add(new ColumnStyle(SizeType.Percent, 5F));
-            tableLayoutPanel1.ColumnStyles.Add(new ColumnStyle(SizeType.Percent, 5F));
-            tableLayoutPanel1.ColumnStyles.Add(new ColumnStyle(SizeType.Percent, 5F));
-            tableLayoutPanel1.ColumnStyles.Add(new ColumnStyle(SizeType.Percent, 5F));
-            tableLayoutPanel1.ColumnStyles.Add(new ColumnStyle(SizeType.Percent, 5F));
-            tableLayoutPanel1.ColumnStyles.Add(new ColumnStyle(SizeType.Percent, 5F));
-            tableLayoutPanel1.ColumnStyles.Add(new ColumnStyle(SizeType.Percent, 5F));
-            tableLayoutPanel1.ColumnStyles.Add(new ColumnStyle(SizeType.Percent, 5F));
-            tableLayoutPanel1.ColumnStyles.Add(new ColumnStyle(SizeType.Percent, 5F));
-            tableLayoutPanel1.ColumnStyles.Add(new ColumnStyle(SizeType.Percent, 5F));
-            tableLayoutPanel1.ColumnStyles.Add(new ColumnStyle(SizeType.Percent, 5F));
-            tableLayoutPanel1.ColumnStyles.Add(new ColumnStyle(SizeType.Percent, 5F));
-            tableLayoutPanel1.ColumnStyles.Add(new ColumnStyle(SizeType.Percent, 5F));
-            tableLayoutPanel1.ColumnStyles.Add(new ColumnStyle(SizeType.Percent, 5F));
-            tableLayoutPanel1.ColumnStyles.Add(new ColumnStyle(SizeType.Percent, 5F));
-            tableLayoutPanel1.ColumnStyles.Add(new ColumnStyle(SizeType.Percent, 5F));
-            tableLayoutPanel1.ColumnStyles.Add(new ColumnStyle(SizeType.Percent, 5F));
-            tableLayoutPanel1.ColumnStyles.Add(new ColumnStyle(SizeType.Percent, 5F));
-            tableLayoutPanel1.ColumnStyles.Add(new ColumnStyle(SizeType.Percent, 5F));
-            tableLayoutPanel1.ColumnStyles.Add(new ColumnStyle(SizeType.Percent, 5F));
-            tableLayoutPanel1.Dock = DockStyle.Fill;
-            tableLayoutPanel1.Location = new Point(0, 0);
-            tableLayoutPanel1.Name = "tableLayoutPanel1";
-            tableLayoutPanel1.RowCount = 20;
-            tableLayoutPanel1.RowStyles.Add(new RowStyle(SizeType.Percent, 5F));
-            tableLayoutPanel1.RowStyles.Add(new RowStyle(SizeType.Percent, 5F));
-            tableLayoutPanel1.RowStyles.Add(new RowStyle(SizeType.Percent, 5F));
-            tableLayoutPanel1.RowStyles.Add(new RowStyle(SizeType.Percent, 5F));
-            tableLayoutPanel1.RowStyles.Add(new RowStyle(SizeType.Percent, 5F));
-            tableLayoutPanel1.RowStyles.Add(new RowStyle(SizeType.Percent, 5F));
-            tableLayoutPanel1.RowStyles.Add(new RowStyle(SizeType.Percent, 5F));
-            tableLayoutPanel1.RowStyles.Add(new RowStyle(SizeType.Percent, 5F));
-            tableLayoutPanel1.RowStyles.Add(new RowStyle(SizeType.Percent, 5F));
-            tableLayoutPanel1.RowStyles.Add(new RowStyle(SizeType.Percent, 5F));
-            tableLayoutPanel1.RowStyles.Add(new RowStyle(SizeType.Percent, 5F));
-            tableLayoutPanel1.RowStyles.Add(new RowStyle(SizeType.Percent, 5F));
-            tableLayoutPanel1.RowStyles.Add(new RowStyle(SizeType.Percent, 5F));
-            tableLayoutPanel1.RowStyles.Add(new RowStyle(SizeType.Percent, 5F));
-            tableLayoutPanel1.RowStyles.Add(new RowStyle(SizeType.Percent, 5F));
-            tableLayoutPanel1.RowStyles.Add(new RowStyle(SizeType.Percent, 5F));
-            tableLayoutPanel1.RowStyles.Add(new RowStyle(SizeType.Percent, 5F));
-            tableLayoutPanel1.RowStyles.Add(new RowStyle(SizeType.Percent, 5F));
-            tableLayoutPanel1.RowStyles.Add(new RowStyle(SizeType.Percent, 5F));
-            tableLayoutPanel1.RowStyles.Add(new RowStyle(SizeType.Percent, 5F));
-            tableLayoutPanel1.Size = new Size(800, 800);
-            tableLayoutPanel1.TabIndex = 0;
-            // 
-            // timer1
-            // 
-            timer1.Enabled = true;
-            timer1.Interval = 100;
-            timer1.Tick += ChooseDirection;
-            // 
-            // Form1
-            // 
-            AutoScaleDimensions = new SizeF(7F, 15F);
-            AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(800, 800);
-            Controls.Add(tableLayoutPanel1);
-            Name = "Form1";
-            Text = "Form1";
-            KeyDown += Form1_KeyDown;
-            ResumeLayout(false);
-        }
+     /// <summary>
+     ///  Required method for Designer support - do not modify
+     ///  the contents of this method with the code editor.
+     /// </summary>
+     private void InitializeComponent()
+     {
+         components = new System.ComponentModel.Container();
+         tableLayoutPanel1 = new TableLayoutPanel();
+         timer1 = new System.Windows.Forms.Timer(components);
+         SuspendLayout();
+         // 
+         // tableLayoutPanel1
+         // 
 
-        #endregion
+         tableLayoutPanel1.ColumnCount = 20;
+         tableLayoutPanel1.RowCount = 20;
 
-        public TableLayoutPanel tableLayoutPanel1;
-        private System.Windows.Forms.Timer timer1;
-    }
+         for (int i = 0; i < tableLayoutPanel1.ColumnCount; i++)
+         {
+             tableLayoutPanel1.ColumnStyles.Add(new ColumnStyle(SizeType.Percent, 100F / tableLayoutPanel1.ColumnCount));
+         }
+
+         for (int i = 0; i < tableLayoutPanel1.RowCount; i++)
+         {
+             tableLayoutPanel1.RowStyles.Add(new ColumnStyle(SizeType.Percent, 100F / tableLayoutPanel1.RowCount));
+         }
+
+         tableLayoutPanel1.Dock = DockStyle.Fill;
+         tableLayoutPanel1.Location = new Point(0, 0);
+         tableLayoutPanel1.Name = "tableLayoutPanel1";
+         tableLayoutPanel1.Size = new Size(800, 800);
+         tableLayoutPanel1.TabIndex = 0;
+
+         // 
+         // timer1
+         // 
+         timer1.Enabled = false;
+         timer1.Interval = 100;
+         timer1.Tick += NextFrame;
+         // 
+         // Form1
+         // 
+         AutoScaleDimensions = new SizeF(7F, 15F);
+         AutoScaleMode = AutoScaleMode.Font;
+         ClientSize = new Size(800, 800);
+         Controls.Add(tableLayoutPanel1);
+         Name = "Form1";
+         Text = "Form1";
+         KeyDown += Form1_KeyDown;
+         ResumeLayout(false);
+     }
+
+     #endregion
+
+     public TableLayoutPanel tableLayoutPanel1;
+     private System.Windows.Forms.Timer timer1;
+     }
 }
