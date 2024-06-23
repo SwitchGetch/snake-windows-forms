88a5f85aafcc2c2ba2b0ce1500f58d2ad5f1f571
GetChara() <133586791+SwitchGetch@users.noreply.github.com>
2024-06-23 15:54:53 +0300

    Update Menu.cs

--- Menu.cs
+++ Menu.cs
@@ -1,16 +1,26 @@
-﻿namespace snake
+namespace snake
 {
     public partial class Menu : Form
     {
         public Menu()
         {
             InitializeComponent();
+
+            numericUpDown1.Value = Config.FieldWidth;
+            numericUpDown2.Value = Config.FieldHeight;
+            numericUpDown3.Value = Config.SnakeSpeed;
+            numericUpDown4.Value = Config.WindowWidth;
+            numericUpDown5.Value = Config.WindowHeight;
         }
 
         private void button1_Click(object sender, EventArgs e)
         {
+            Config.WindowWidth = (int)numericUpDown4.Value;
+            Config.WindowHeight = (int)numericUpDown5.Value;
+
             Config.FieldWidth = (int)numericUpDown1.Value;
             Config.FieldHeight = (int)numericUpDown2.Value;
+
             Config.SnakeSpeed = (int)numericUpDown3.Value;
 
             Hide();
