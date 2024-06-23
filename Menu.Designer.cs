3dc3b0a0008d6b1f346a418f5cee0f1e10d81bb6
GetChara() <133586791+SwitchGetch@users.noreply.github.com>
2024-06-23 15:54:38 +0300

    Update Menu.Designer.cs

--- Menu.Designer.cs
+++ Menu.Designer.cs
@@ -37,10 +37,16 @@ namespace snake
             numericUpDown2 = new NumericUpDown();
             numericUpDown3 = new NumericUpDown();
             button1 = new Button();
+            label5 = new Label();
+            label6 = new Label();
+            numericUpDown4 = new NumericUpDown();
+            numericUpDown5 = new NumericUpDown();
             tableLayoutPanel1.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)numericUpDown1).BeginInit();
             ((System.ComponentModel.ISupportInitialize)numericUpDown2).BeginInit();
             ((System.ComponentModel.ISupportInitialize)numericUpDown3).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)numericUpDown4).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)numericUpDown5).BeginInit();
             SuspendLayout();
             // 
             // tableLayoutPanel1
@@ -57,13 +63,17 @@ namespace snake
             tableLayoutPanel1.ColumnStyles.Add(new ColumnStyle(SizeType.Percent, 10F));
             tableLayoutPanel1.ColumnStyles.Add(new ColumnStyle(SizeType.Percent, 10F));
             tableLayoutPanel1.Controls.Add(label1, 4, 1);
-            tableLayoutPanel1.Controls.Add(numericUpDown1, 5, 3);
-            tableLayoutPanel1.Controls.Add(label2, 4, 3);
-            tableLayoutPanel1.Controls.Add(label3, 4, 4);
-            tableLayoutPanel1.Controls.Add(label4, 4, 5);
-            tableLayoutPanel1.Controls.Add(numericUpDown2, 5, 4);
-            tableLayoutPanel1.Controls.Add(numericUpDown3, 5, 5);
+            tableLayoutPanel1.Controls.Add(numericUpDown1, 5, 4);
+            tableLayoutPanel1.Controls.Add(label2, 4, 4);
+            tableLayoutPanel1.Controls.Add(label3, 4, 5);
+            tableLayoutPanel1.Controls.Add(label4, 4, 6);
+            tableLayoutPanel1.Controls.Add(numericUpDown2, 5, 5);
+            tableLayoutPanel1.Controls.Add(numericUpDown3, 5, 6);
             tableLayoutPanel1.Controls.Add(button1, 4, 7);
+            tableLayoutPanel1.Controls.Add(label5, 4, 2);
+            tableLayoutPanel1.Controls.Add(label6, 4, 3);
+            tableLayoutPanel1.Controls.Add(numericUpDown4, 5, 2);
+            tableLayoutPanel1.Controls.Add(numericUpDown5, 5, 3);
             tableLayoutPanel1.Dock = DockStyle.Fill;
             tableLayoutPanel1.Location = new System.Drawing.Point(0, 0);
             tableLayoutPanel1.Name = "tableLayoutPanel1";
@@ -96,72 +106,72 @@ namespace snake
             label1.Text = "SNAKE";
             label1.TextAlign = ContentAlignment.MiddleCenter;
             // 
+            // numericUpDown1
+            // 
+            numericUpDown1.Dock = DockStyle.Fill;
+            numericUpDown1.Location = new System.Drawing.Point(403, 183);
+            numericUpDown1.Maximum = new decimal(new int[] { 50, 0, 0, 0 });
+            numericUpDown1.Minimum = new decimal(new int[] { 2, 0, 0, 0 });
+            numericUpDown1.Name = "numericUpDown1";
+            numericUpDown1.Size = new Size(74, 23);
+            numericUpDown1.TabIndex = 4;
+            numericUpDown1.Value = new decimal(new int[] { 2, 0, 0, 0 });
+            // 
             // label2
             // 
             label2.AutoSize = true;
             label2.Dock = DockStyle.Fill;
-            label2.Location = new System.Drawing.Point(323, 135);
+            label2.Location = new System.Drawing.Point(323, 180);
             label2.Name = "label2";
             label2.Size = new Size(74, 45);
             label2.TabIndex = 1;
-            label2.Text = "Field Width";
+            label2.Text = "Field\r\nWidth";
             label2.TextAlign = ContentAlignment.MiddleCenter;
             // 
             // label3
             // 
             label3.AutoSize = true;
             label3.Dock = DockStyle.Fill;
-            label3.Location = new System.Drawing.Point(323, 180);
+            label3.Location = new System.Drawing.Point(323, 225);
             label3.Name = "label3";
             label3.Size = new Size(74, 45);
             label3.TabIndex = 2;
-            label3.Text = "Field Height";
+            label3.Text = "Field\r\nHeight";
             label3.TextAlign = ContentAlignment.MiddleCenter;
             // 
             // label4
             // 
             label4.AutoSize = true;
             label4.Dock = DockStyle.Fill;
-            label4.Location = new System.Drawing.Point(323, 225);
+            label4.Location = new System.Drawing.Point(323, 270);
             label4.Name = "label4";
             label4.Size = new Size(74, 45);
             label4.TabIndex = 3;
-            label4.Text = "Snake Speed";
+            label4.Text = "Snake\r\nSpeed";
             label4.TextAlign = ContentAlignment.MiddleCenter;
-            // 
-            // numericUpDown1
-            // 
-            numericUpDown1.Dock = DockStyle.Fill;
-            numericUpDown1.Location = new System.Drawing.Point(403, 138);
-            numericUpDown1.Maximum = new decimal(new int[] { 50, 0, 0, 0 });
-            numericUpDown1.Minimum = new decimal(new int[] { 2, 0, 0, 0 });
-            numericUpDown1.Name = "numericUpDown1";
-            numericUpDown1.Size = new Size(74, 23);
-            numericUpDown1.TabIndex = 4;
-            numericUpDown1.Value = new decimal(new int[] { Config.FieldWidth, 0, 0, 0 });
             // 
             // numericUpDown2
             // 
             numericUpDown2.Dock = DockStyle.Fill;
-            numericUpDown2.Location = new System.Drawing.Point(403, 183);
+            numericUpDown2.Location = new System.Drawing.Point(403, 228);
             numericUpDown2.Maximum = new decimal(new int[] { 50, 0, 0, 0 });
             numericUpDown2.Minimum = new decimal(new int[] { 2, 0, 0, 0 });
             numericUpDown2.Name = "numericUpDown2";
             numericUpDown2.Size = new Size(74, 23);
             numericUpDown2.TabIndex = 5;
-            numericUpDown2.Value = new decimal(new int[] { Config.FieldHeight, 0, 0, 0 });
+            numericUpDown2.Value = new decimal(new int[] { 2, 0, 0, 0 });
             // 
             // numericUpDown3
             // 
             numericUpDown3.Dock = DockStyle.Fill;
             numericUpDown3.Increment = new decimal(new int[] { 100, 0, 0, 0 });
-            numericUpDown3.Location = new System.Drawing.Point(403, 228);
+            numericUpDown3.Location = new System.Drawing.Point(403, 273);
             numericUpDown3.Maximum = new decimal(new int[] { 999, 0, 0, 0 });
             numericUpDown3.Minimum = new decimal(new int[] { 1, 0, 0, 0 });
             numericUpDown3.Name = "numericUpDown3";
             numericUpDown3.Size = new Size(74, 23);
             numericUpDown3.TabIndex = 6;
-            numericUpDown3.Value = new decimal(new int[] { Config.SnakeSpeed, 0, 0, 0 });
+            numericUpDown3.Value = new decimal(new int[] { 1, 0, 0, 0 });
             // 
             // button1
             // 
@@ -176,6 +186,50 @@ namespace snake
             button1.UseVisualStyleBackColor = true;
             button1.Click += button1_Click;
             // 
+            // label5
+            // 
+            label5.AutoSize = true;
+            label5.Dock = DockStyle.Fill;
+            label5.Location = new System.Drawing.Point(323, 90);
+            label5.Name = "label5";
+            label5.Size = new Size(74, 45);
+            label5.TabIndex = 8;
+            label5.Text = "Window Width";
+            label5.TextAlign = ContentAlignment.MiddleCenter;
+            // 
+            // label6
+            // 
+            label6.AutoSize = true;
+            label6.Dock = DockStyle.Fill;
+            label6.Location = new System.Drawing.Point(323, 135);
+            label6.Name = "label6";
+            label6.Size = new Size(74, 45);
+            label6.TabIndex = 9;
+            label6.Text = "Window\r\nHeight";
+            label6.TextAlign = ContentAlignment.MiddleCenter;
+            // 
+            // numericUpDown4
+            // 
+            numericUpDown4.Dock = DockStyle.Fill;
+            numericUpDown4.Location = new System.Drawing.Point(403, 93);
+            numericUpDown4.Maximum = new decimal(new int[] { 1920, 0, 0, 0 });
+            numericUpDown4.Minimum = new decimal(new int[] { 1, 0, 0, 0 });
+            numericUpDown4.Name = "numericUpDown4";
+            numericUpDown4.Size = new Size(74, 23);
+            numericUpDown4.TabIndex = 10;
+            numericUpDown4.Value = new decimal(new int[] { 1, 0, 0, 0 });
+            // 
+            // numericUpDown5
+            // 
+            numericUpDown5.Dock = DockStyle.Fill;
+            numericUpDown5.Location = new System.Drawing.Point(403, 138);
+            numericUpDown5.Maximum = new decimal(new int[] { 1080, 0, 0, 0 });
+            numericUpDown5.Minimum = new decimal(new int[] { 1, 0, 0, 0 });
+            numericUpDown5.Name = "numericUpDown5";
+            numericUpDown5.Size = new Size(74, 23);
+            numericUpDown5.TabIndex = 11;
+            numericUpDown5.Value = new decimal(new int[] { 1, 0, 0, 0 });
+            // 
             // Menu
             // 
             AutoScaleDimensions = new SizeF(7F, 15F);
@@ -189,6 +243,8 @@ namespace snake
             ((System.ComponentModel.ISupportInitialize)numericUpDown1).EndInit();
             ((System.ComponentModel.ISupportInitialize)numericUpDown2).EndInit();
             ((System.ComponentModel.ISupportInitialize)numericUpDown3).EndInit();
+            ((System.ComponentModel.ISupportInitialize)numericUpDown4).EndInit();
+            ((System.ComponentModel.ISupportInitialize)numericUpDown5).EndInit();
             ResumeLayout(false);
         }
 
@@ -203,5 +259,9 @@ namespace snake
         private NumericUpDown numericUpDown2;
         private NumericUpDown numericUpDown3;
         private Button button1;
+        private Label label5;
+        private Label label6;
+        private NumericUpDown numericUpDown4;
+        private NumericUpDown numericUpDown5;
     }
 }
